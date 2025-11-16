22ab51e8a4a00982c142125567e88b7a885cbb78
이재우 <jw1212@ijaeuui-MacBookAir.local>
2025-11-16 15:01:28 +0900

    feat: FIP 랭킹 서비스 API 뼈대 완성

---
+++ src/main/java/com/dlwodn/kbo_savermatrix_system/KboSavermatrixSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dlwodn.kbo_savermatrix_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KboSavermatrixSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KboSavermatrixSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dlwodn/kbo_savermatrix_system/KboSavermatrixSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlwodn.kbo_savermatrix_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KboSavermatrixSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
